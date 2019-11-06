99cc9fc2a10d0434d575108cbfab4f796557a1c0
sofia abdurakhmanova <sofiya.abdurahma@gmail.com>
2019-11-06 19:48:18 +0200

    Add Customer model tests

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,83 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:customer) { FactoryBot.create(:customer) }
+
+  describe "valid factory" do
+    it "is valid with valid attributes" do
+      expect(customer).to be_valid
+    end
+  end
+
+  describe "validation" do
+    context "when first name" do
+      it "is blank" do
+        expect(build(:customer, first_name: "")).not_to be_valid
+      end
+
+      it "is shorter than 2 characters" do
+        expect(build(:customer, first_name: 'a')).not_to be_valid
+      end
+
+      it "is 2 characters long" do
+        expect(build(:customer, first_name: 'ab')).to be_valid
+      end
+
+      it "includes other symbols than characters" do
+        expect(build(:customer, first_name: 'a5b')).not_to be_valid
+        expect(build(:customer, first_name: '*a$b')).not_to be_valid
+      end
+    end
+
+    context "when last name" do
+      it "is blank" do
+        expect(build(:customer, last_name: "")).not_to be_valid
+      end
+
+      it "is shorter than 2 characters" do
+        expect(build(:customer, last_name: 'a')).not_to be_valid
+      end
+
+      it "is 2 characters long" do
+        expect(build(:customer, last_name: 'ab')).to be_valid
+      end
+
+      it "includes other symbols than characters" do
+        expect(build(:customer, last_name: 'ab1')).not_to be_valid
+        expect(build(:customer, last_name: 'a#b')).not_to be_valid
+      end
+    end
+
+    context "when email" do
+      it "is blank" do
+        expect(build(:customer, email: '')).not_to be_valid
+      end
+
+      it { expect(customer).to_not allow_value("base@example").for(:email) }
+
+      it { expect(customer).to_not allow_value("base.example.com").for(:email) }
+
+      it { should validate_uniqueness_of(:email) }
+    end
+
+    context "when date of birth" do
+      it "is blank" do
+        expect(build(:customer, date_of_birth: '')).not_to be_valid
+      end
+
+      it 'is between the time range' do
+        expect(customer.date_of_birth.find_age).to be_between(18, 100)
+      end
+
+      it 'is younger than 18 years old' do
+        customer = build(:customer, date_of_birth: '01/01/2005')
+        expect(customer.date_of_birth.find_age).not_to be_between(18, 100)
+      end
+
+      it 'is older than 100 years old' do
+        customer = build(:customer, date_of_birth: '01/01/1908')
+        expect(customer.date_of_birth.find_age).not_to be_between(18, 100)
+      end
+    end
+  end
 end
