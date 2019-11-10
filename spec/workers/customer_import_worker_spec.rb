8538388c2488dfba9751bfeb3b650478352fe340
sofia abdurakhmanova <sofiya.abdurahma@gmail.com>
2019-11-10 10:58:15 +0200

    Add tests to CustomerImportWorker

--- spec/workers/customer_import_worker_spec.rb
+++ spec/workers/customer_import_worker_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe CustomerImportWorker, type: :worker do
       FactoryBot.create(:import_detail, import_id: import.id)
     end
 
-    describe 'Worker' do
+    describe 'Worker itself' do
       Sidekiq::Testing.fake! do
         it 'should respond to #perform' do
           expect(CustomerImportWorker.new).to respond_to(:perform)
@@ -24,7 +24,23 @@ RSpec.describe CustomerImportWorker, type: :worker do
       end
     end
 
-    describe 'Import Detail' do
+    describe 'for Customer' do
+      let(:now) { Time.now }
+      before do
+        Sidekiq::Worker.clear_all
+        Timecop.freeze(now) do
+          Sidekiq::Testing.inline! do
+            CustomerImportWorker.perform_async(import_detail.id)
+          end
+        end
+      end
+
+      it 'should include one item' do
+        expect(Customer.count).to eq(1)
+      end
+    end
+
+    describe 'for Import Detail' do
       let(:now) { Time.now }
       before do
         Sidekiq::Worker.clear_all
@@ -37,7 +53,17 @@ RSpec.describe CustomerImportWorker, type: :worker do
 
       it 'should change import_detail status to started' do
         import_detail.reload
-        expect(import_detail.import_status).to eq('started')
+        expect(import_detail.import_status).to eq('completed')
+      end
+
+      it 'should change import_detail status to started' do
+        import_detail.reload
+        expect(import_detail.created_customers_amount).to eq(1)
+      end
+
+      it 'should change import_detail status to started' do
+        import_detail.reload
+        expect(import_detail.rejected_customers_amount).to eq(13)
       end
     end
   end
